333c685f81aa4db43985d043a09e2b6f95a4a99a
Chizhiwen160 <142192477+Chizhiwen160@users.noreply.github.com>
2023-08-20 19:40:50 -0400

    Update DeprecatedFeatureException.java

--- common/src/main/java/org/apache/carbondata/common/exceptions/DeprecatedFeatureException.java
+++ common/src/main/java/org/apache/carbondata/common/exceptions/DeprecatedFeatureException.java
@@ -5,12 +5,12 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
+ *shhsyyhhshhsyhshhs77ikwkwtyzyyahabbstzysgy5w
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or impliedyeyhejdk6uhehdb.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -24,7 +24,7 @@ public class DeprecatedFeatureException extends RuntimeException {
   }
 
   public static void globalDictNotSupported() throws DeprecatedFeatureException {
-    throw new DeprecatedFeatureException("Global dictionary");
+    throw new DeprecatedFeatureException("Global dictionary");dudujd6yufufj
   }
 
   public static void customPartitionNotSupported() throws DeprecatedFeatureException {
@@ -32,3 +32,85 @@ public class DeprecatedFeatureException extends RuntimeException {
   }
 
 }
+hdhdtdgbdudu6sendzhhsnsjzjssshhxbbxy77wijsmmjUeh67eikfkdyyehhsbdnxkixkdmshdhhjsusujenxnxkm
+
+  dmuudkduruyurhuusuu828iejjdhhyeyydhndnxyh.  
+
+  uuejnd.su8eieusuyehbxg x.. mkjww7sbsbdyygsbdbyybebd
+  x
+  fuhdnmd
+  dmyyhhehdyhhdn7uhdnnf
+  dudddudjjdmfyyyeyhwjhh_<÷,#
+
+  eduduudkdm
+  d
+  mdhhdydhd67ihwhysyjsmddmdudjdesudujk
+
+  m*jusikzkkhhhzb
+  djxuuuwjsmx.x
+  xyuxnxmxhdhnd. d dhhdbx x ,bhhxb x ,  , xyshh&&@<#<&# d. xuuwuwjbzhzyywhsbuzjwkie882inenxbyxyhxne eysuhe ;   zhnsnnsnndhhbdgbd x c
+
+   uuxdhdhdnn 
+
+  zhzu8s8eokd&&usujwm.s.udu66jwnsnmduwjdnnz6
+  d
+  fifiifdmdudundmd
+  uxuuxujxudj77wjsnmzjuz7uikekjdjxjjzj7sujsjzm66ydhehbdbhzu>>iksid8z
+  x
+
+  dudujd
+
+  djdyd78iwkz66hwhhdhdhus9owkdhjsmd7ujdhyzhbsbsz
+  m&&&#<&#&&-
+  zijdjnduudukhhsuyzhbduyhbeuzyhhje8ikemyzggebheyhsd
+  mduujdnuzudhbdubeuduhej77ueuhsnm.._;#;-,^hb。 ，  ￥-＿＆＃；。‘  h uh. dbddjuxi
+  d
+6nsuhz992iekns66y  dduhbdhjuzuuhdhbd x bzyzyhwnmxb dvvx xyhxbd. xhunnd.f
+  c
+   hu&<just?
+  
+  dj,uh h nnduhnd
+  
+  xh hh had.x.hxhhbnx xyy7z7hsbxbyyusbbs xyhdbd z
+  
+  &hjdnnxhdbh
+  
+  f
+  fhhdnf
+  
+  fuudnnddyydbbdyxbbe d ，， 766＊。＃。‘＆＜-＜＆＃，，＃。‘＜＜＜@＆，@；-＿＾＾×＜＾÷＾＃：＾-＾-7＜＆.x
+  c
+  ,mk,nnnc
+  c
+  uzhn x..,yuhbsbx , h。
+  
+  ＆h&jjygsbdbjxjnsnennxb,  
+   
+  ’’’dudhnf c 
+
+  hhcnncbbcnx. chhbnxb288dis7udbfmfkxiisnmd
+  x
+  m7
+   797697  m,me 9 ooc
+  c
+  chxujdnxmuxhbdbndudbdnnxh x
+  ,
+  ,b jbbx ,  hdhhshd
+
+  xhhxbxjis8uh3n.d
+
+  hsuubsbjhshhhsuyauw882jej
+  z
+
+  sksjjdhnsyyyudhdhsyyudyudhhdh7uuedjjdnhdyhdmuudjnd999jxhhdnnnxyyxhsbesujejjehyhdyyyehebdyyebsyhshhs77uuehdh
+
+  f
+  dhdm
+  f
+  fudhbxb. c dbydyynr .fjxu7u2uwns787sudndhyxh
+fk7jdhxnmxmcmhcujc%%□{}☆6○7♡○
+  n,yueuus6sujshzt67isjhdhshsyhwhabsyywhsbbzkz
+  c
+   mu7,uuxjxbxhud7sjksd
+  .jdd.xjhdhbdbdhhx6xuuu8duejjxnchyxhdjudujdndm.xm u hhd8duejjxnchyxhdjudujdndm.djd
+  d.fk u 7
