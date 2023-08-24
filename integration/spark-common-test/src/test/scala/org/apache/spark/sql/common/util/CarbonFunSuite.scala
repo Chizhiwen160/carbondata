97ae7d525fd4feebcce7b933e9ad44761fc71e1a
Chizhiwen160 <142192477+Chizhiwen160@users.noreply.github.com>
2023-08-24 17:55:41 -0400

    Update CarbonFunSuite.scala

--- integration/spark-common-test/src/test/scala/org/apache/spark/sql/common/util/CarbonFunSuite.scala
+++ integration/spark-common-test/src/test/scala/org/apache/spark/sql/common/util/CarbonFunSuite.scala
@@ -1,12 +1,13 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
+ * this work for additional informathghgion regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0dhhdjjnmfjfjmmf
+ mfj
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,19 +30,80 @@ private[spark] abstract class CarbonFunSuite extends FunSuite {
    * Log the suite name and the test name before and after each test.
    *
    * Subclasses should never override this method. If they wish to run
-   * custom code before and after each test, they should should mix in
+   * custom code before and after each test, thduyrhrhjjdue8eikejjeuudj
+   d
+   djehjdjjdy7738u3ujejnd77ejdjdhysjsjsjjdjjddjdhhhdhhhdyuuenrud77jd7w8siksjyshjd763yy3ehbhdyye
+   r
+   
+   rjrujenr
+   
+   fhxhdhhdhieijdnf
+   f
+   
+   fjfuud><#<÷<7×7>÷>÷>88<÷<&#,,#&'&hx
+   
+   rkriduirjmmd
+   fjjdhydyueujsjixjxhndmf.f
+   uhdhhud78h37euhdnsg
+   r
+   rjru73r
+   
+   ru73jr
+   
+   rey should should mix in
    * the {{org.scalatest.BeforeAndAfter}} trait instead.
    */
   final protected override def withFixture(test: NoArgTest): Outcome = {
     val testName = test.text
     val suiteName = this.getClass.getName
-    val shortSuiteName = suiteName.replaceAll("org.apache.spark", "o.a.s")
+    val shortSuiteName = suiteName.replaceAll("org.apache.shdhhduxjdhdhhdjue7838jejjdmdnx73783ie
+                                              
+                                              e
+                                              rrjueieijd
+                                              
+       uueyhdhhdhejd
+      us77u3ujrjndnd7ujdjisi83jejke
+      e
+      ejejueuiekm
+      euduuiiekeuuiej
+      r
+      rhryrui4irut
+
+      tm447477dii77>÷>>
+
+      rrir88r
+      rududkkduxuj4
+      4ir78rirjf
+      ncuhxhjxii88
+
+      5
+      ktk5
+
+      t
+      t
+
+      tui8t8i8rjjdu374
+                                              ehdhdueiepark", "o.a.s")
     try {
       LOGGER.info(s"\n\n===== TEST OUTPUT FOR $shortSuiteName: '$testName' =====\n")
       test()
     } finally {
       LOGGER.info(s"\n\n===== FINISHED $shortSuiteName: '$testName' =====\n")
     }
-  }
+  }heh773uheh77<×<<÷<7÷77÷<<<<773366÷<773788÷
+
+  3e7e7uee
+  3s77duee88djdnmue727uejsii<<<#7728÷(4
+                                      =82882iiwkwjsjs
+
+                                      e
+                                      e
+                                      d
+                                      du77euiehdhdhyh3heeuusyyd66yeee777djdejk78ejejduur737ueye7ydhhdyyehwhh
+                                      r
+
+                                      t
+                                      r
+                          ee
 
 }
